f86a5d26fa565b6b0c69f5471edef092eb71e463
andros21 <andrea.ros21@e.email>
2022-05-23 12:17:54 +0200

    build(pgrank): bump version from 0.1.1 to 0.1.2

--- include/json2mat/json2mat.hpp
+++ include/json2mat/json2mat.hpp
@@ -1,7 +1,7 @@
 /**
  * @file
  * @authors Andrea Rossoni
- * @version 0.1.1
+ * @version 0.1.2
  *
  * @subsubsection Description
  *
--- src/pagerank/pagerank.cpp
+++ src/pagerank/pagerank.cpp
@@ -1,6 +1,6 @@
 /**
  * @authors Andrea Rossoni
- * @version 0.1.1
+ * @version 0.1.2
  *
  */
 
